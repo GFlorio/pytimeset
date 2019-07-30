d757ab10d245c00e6a48bec85242fad7aaf2dc8e
Gabriel Florio <gabriel@gabrielflorio.com>
2019-07-30 15:54:52 -0300

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='pytimeset',
+    version='0.1',
+    packages=['timeset'],
+    url='https://github.com/GFlorio/pytimeset',
+    license='MIT',
+    author='Gabriel Florio',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author_email='gabriel@gabrielflorio.com',
+    description='Defines sets and intervals to work with time, and provides arithmetic operations '
+                'for them. '
+)
