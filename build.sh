12802190f5f45c36f3d36b26da98354648a46540
Gabriel Florio <gabriel@gabrielflorio.com>
2019-08-05 07:21:28 -0300

    Added publishing support to build script

--- build.sh
+++ build.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
 set -e
 
-cython timeset.pyx
+echo "### CYTHON ###"
+cython -a timeset/timeset.pyx
+echo "### SETUPTOOLS ###"
 python3 setup.py build_ext --inplace
+echo "### TESTING ###"
+python3 -m unittest discover -s tests
+
+if [ "$1" == "publish" ]; then
+  echo "### BUILDING WHEEL ###"
+  python3 setup.py sdist bdist_wheel
+  echo "### PUBLISHING ###"
+  python3 -m twine upload dist/*
+fi
