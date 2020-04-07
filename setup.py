00f78976e36fcff6ceb11d134e6e7ffb720961e5
Gabriel Florio <gabriel@gabrielflorio.com>
2020-04-07 13:10:33 -0300

    Add more info in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='pytimeset',
-    version='0.5.9',
+    version='0.5.10',
     packages=['timeset'],
     url='https://github.com/GFlorio/pytimeset',
     license='MIT',
@@ -17,4 +17,25 @@ setup(
                 'for them. Uses Cython extensions for performance.',
     ext_modules=[Extension('timeset.timeset', ['timeset/timeset.c'])],
     package_data={'timeset': ['timeset.pyi', 'py.typed']},
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 4 - Beta',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+
+        # Pick your license as you wish (should match "license" above)
+         'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
 )
